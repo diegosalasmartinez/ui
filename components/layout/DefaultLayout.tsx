f3aaa061c541b345feb494a393cffa1f4902a626
Diego Salas <Diego.Salas@promart.pe>
2023-02-06 15:10:55 -0500

    Fix default page imports

--- components/layout/DefaultLayout.tsx
+++ components/layout/DefaultLayout.tsx
@@ -1,14 +1,15 @@
 import Head from 'next/head'
+import { QuickNavSectionInterface } from '@/interfaces/QuickNav'
 import QuickNav from './QuickNav'
 import Sidebar from './Sidebar'
 
 interface DefaultLayoutProps {
   title?: string
-  loading?: boolean
+  nav?: QuickNavSectionInterface[]
   children: React.ReactNode
 }
 
-export default function DefaultLayout({ title = 'My Planner', loading = false, children }: DefaultLayoutProps) {
+export default function DefaultLayout({ title = 'My Planner', nav, children }: DefaultLayoutProps) {
   return (
     <div className='flex min-h-screen min-w-full flex-col items-center'>
       <Head>
@@ -18,10 +19,9 @@ export default function DefaultLayout({ title = 'My Planner', loading = false, c
       <div className='flex h-screen w-screen'>
         <Sidebar />
         <div className='w-full overflow-y-auto bg-gray-200'>
-          {loading && <div>Loading...</div>}
           <div className='my-0 mx-auto flex w-11/12 max-w-7xl flex-col justify-center py-4'>{children}</div>
         </div>
-        <QuickNav />
+        <QuickNav nav={nav}/>
       </div>
     </div>
   )
--- pages/card/index.tsx
+++ pages/card/index.tsx
@@ -2,21 +2,81 @@ import { cn } from '@/utils/style'
 import BasicCard from '@/components/card/BasicCard'
 import DefaultLayout from '@/components/layout/DefaultLayout'
 
+const nav = [
+  { title: 'Installation', children: [] },
+  { title: 'Usage', children: [] },
+  {
+    title: 'Examples',
+    children: [
+      { title: 'With text', href: 'basic-cards', children: [] },
+      { title: 'Disabled', children: [] },
+    ],
+  },
+]
+
 export default function Card() {
   return (
-    <DefaultLayout title='UI Design System'>
-      <h1>Basic Cards</h1>
-      <div className={cn('grid grid-cols-1 gap-4', 'md:grid-cols-2')}>
-        <BasicCard
-          title='Velit et ullamco nisi elit voluptate'
-          body='Veniam proident in anim ea velit minim laborum voluptate ullamco mollit exercitation qui reprehenderit excepteur. Pariatur irure cillum incididunt veniam ea adipisicing veniam sint anim ut. Quis labore reprehenderit minim dolor non et consectetur elit ea ea.'
-        />
-        <BasicCard title='Deserunt consectetur labore exercitation'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt repudiandae suscipit iste aperiam labore?
-          <span className='font-bold'> Lorem ipsum dolor sit amet consectet? </span>
-          Magni error perferendis rerum corrupti accusamus cum itaque ducimus asperiores, sequi doloribus fuga nam earum
-          nisi.
-        </BasicCard>
+    <DefaultLayout title='UI Design System' nav={nav}>
+      <div className='mb-4'>
+        <h1 id='a-cards'>Complex Cards</h1>
+        <div className={cn('grid grid-cols-1 gap-4', 'md:grid-cols-2')}>
+          <BasicCard
+            title='Velit et ullamco nisi elit voluptate'
+            body='Veniam proident in anim ea velit minim laborum voluptate ullamco mollit exercitation qui reprehenderit excepteur. Pariatur irure cillum incididunt veniam ea adipisicing veniam sint anim ut. Quis labore reprehenderit minim dolor non et consectetur elit ea ea.'
+          />
+          <BasicCard title='Deserunt consectetur labore exercitation'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt repudiandae suscipit iste aperiam labore?
+            <span className='font-bold'> Lorem ipsum dolor sit amet consectet? </span>
+            Magni error perferendis rerum corrupti accusamus cum itaque ducimus asperiores, sequi doloribus fuga nam
+            earum nisi.
+          </BasicCard>
+        </div>
+      </div>
+      <div className='mb-4'>
+        <h1 id='b-cards'>Complex Cards</h1>
+        <div className={cn('grid grid-cols-1 gap-4', 'md:grid-cols-2')}>
+          <BasicCard
+            title='Velit et ullamco nisi elit voluptate'
+            body='Veniam proident in anim ea velit minim laborum voluptate ullamco mollit exercitation qui reprehenderit excepteur. Pariatur irure cillum incididunt veniam ea adipisicing veniam sint anim ut. Quis labore reprehenderit minim dolor non et consectetur elit ea ea.'
+          />
+          <BasicCard title='Deserunt consectetur labore exercitation'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt repudiandae suscipit iste aperiam labore?
+            <span className='font-bold'> Lorem ipsum dolor sit amet consectet? </span>
+            Magni error perferendis rerum corrupti accusamus cum itaque ducimus asperiores, sequi doloribus fuga nam
+            earum nisi.
+          </BasicCard>
+        </div>
+      </div>
+
+      <div className='mb-4'>
+        <h1 id='basic-cards'>Basic Cards</h1>
+        <div className={cn('grid grid-cols-1 gap-4', 'md:grid-cols-2')}>
+          <BasicCard
+            title='Velit et ullamco nisi elit voluptate'
+            body='Veniam proident in anim ea velit minim laborum voluptate ullamco mollit exercitation qui reprehenderit excepteur. Pariatur irure cillum incididunt veniam ea adipisicing veniam sint anim ut. Quis labore reprehenderit minim dolor non et consectetur elit ea ea.'
+          />
+          <BasicCard title='Deserunt consectetur labore exercitation'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt repudiandae suscipit iste aperiam labore?
+            <span className='font-bold'> Lorem ipsum dolor sit amet consectet? </span>
+            Magni error perferendis rerum corrupti accusamus cum itaque ducimus asperiores, sequi doloribus fuga nam
+            earum nisi.
+          </BasicCard>
+        </div>
+      </div>
+      <div className='mb-4'>
+        <h1 id='c-cards'>Complex Cards</h1>
+        <div className={cn('grid grid-cols-1 gap-4', 'md:grid-cols-2')}>
+          <BasicCard
+            title='Velit et ullamco nisi elit voluptate'
+            body='Veniam proident in anim ea velit minim laborum voluptate ullamco mollit exercitation qui reprehenderit excepteur. Pariatur irure cillum incididunt veniam ea adipisicing veniam sint anim ut. Quis labore reprehenderit minim dolor non et consectetur elit ea ea.'
+          />
+          <BasicCard title='Deserunt consectetur labore exercitation'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt repudiandae suscipit iste aperiam labore?
+            <span className='font-bold'> Lorem ipsum dolor sit amet consectet? </span>
+            Magni error perferendis rerum corrupti accusamus cum itaque ducimus asperiores, sequi doloribus fuga nam
+            earum nisi.
+          </BasicCard>
+        </div>
       </div>
     </DefaultLayout>
   )
