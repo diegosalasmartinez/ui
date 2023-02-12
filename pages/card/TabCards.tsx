66db059c0004aad2e17f06a8e9906ba0ee4ef84c
Diego Salas <Diego.Salas@promart.pe>
2023-02-12 16:21:30 -0500

    Add tab cards

---
+++ pages/card/TabCards.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import { cn } from '@/utils/style'
+import TabCard, { TabCardContent } from '@/components/card/TabCard'
+import FlatButton from '@/components/button/FlatButton'
+
+const tabs = [
+  { value: '1', label: 'Page 1' },
+  { value: '2', label: 'Page 2' },
+]
+
+export default function TabCards() {
+  const [tab, setTab] = useState('1')
+
+  return (
+    <div id='tab-cards' className='mb-6'>
+      <h1 className='mb-4 text-xl font-bold'>Tab Cards</h1>
+      <div className={cn('grid grid-cols-1 gap-4', 'md:grid-cols-2')}>
+        <TabCard tabSelected={tab} tabs={tabs} onClick={(value) => setTab(value)}>
+          <TabCardContent value='1' tab={tab}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt repudiandae suscipit iste aperiam labore?
+            <span className='font-bold'> Lorem ipsum dolor sit amet consectet? </span>
+            Magni error perferendis rerum corrupti accusamus cum itaque ducimus asperiores, sequi doloribus fuga nam
+            earum nisi.
+          </TabCardContent>
+          <TabCardContent value='2' tab={tab}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias repellendus, suscipit quos laborum nulla
+            reprehenderit placeat, totam ipsum quas accusantium ipsam quasi obcaecati labore fugit animi, ab illum
+            tempora commodi.
+          </TabCardContent>
+        </TabCard>
+        <TabCard tabSelected={tab} tabs={tabs} onClick={(value) => setTab(value)}>
+          <TabCardContent value='1' tab={tab}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt repudiandae suscipit iste aperiam labore?
+            <span className='font-bold'> Lorem ipsum dolor sit amet consectet? </span>
+            Magni error perferendis rerum corrupti accusamus cum itaque ducimus asperiores, sequi doloribus fuga nam
+            earum nisi.
+          </TabCardContent>
+          <TabCardContent value='2' tab={tab}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea unde eveniet aliquid voluptates facilis libero
+            ex laborum dolores, minus impedit, illo, quibusdam fugit neque voluptatem voluptatum! Eaque velit voluptas
+            ullam.
+            <div className='mt-1 flex justify-end gap-2'>
+              <FlatButton text='Cancel' category='secondary' />
+              <FlatButton text='Accept' />
+            </div>
+          </TabCardContent>
+        </TabCard>
+      </div>
+    </div>
+  )
+}
