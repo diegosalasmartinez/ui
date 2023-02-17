c8f3730044c1e6ad9bc584d69c8fd5bc328cdc74
Diego Salas <Diego.Salas@promart.pe>
2023-02-17 18:25:49 -0500

    Add card with vertical tabs

--- components/card/TabCard.tsx
+++ components/card/TabCard.tsx
@@ -27,14 +27,35 @@ export default function TabCard({ tabSelected, tabs, body, children, onClick }: 
   )
 }
 
+export function TabVerticalCard({ tabSelected, tabs, body, children, onClick }: TabCardProps) {
+  return (
+    <div className={cn('flex', 'rounded-lg bg-white shadow-md', 'dark:bg-gray-800')}>
+      <div className='flex w-80 flex-col'>
+        {tabs.map((item) => (
+          <TabCardItem
+            key={item.value}
+            vertical={true}
+            value={item.value}
+            text={item.label}
+            active={tabSelected === item.value}
+            onClick={onClick}
+          />
+        ))}
+      </div>
+      <p className='my-2 p-4 text-sm text-gray-600 dark:text-gray-300'>{children ?? body}</p>
+    </div>
+  )
+}
+
 interface TabCardItemProps {
   value: string
   text: string
   active: boolean
+  vertical?: boolean
   onClick: (value: string) => void
 }
 
-function TabCardItem({ value, text, active, onClick }: TabCardItemProps) {
+function TabCardItem({ value, text, vertical = false, active, onClick }: TabCardItemProps) {
   const handleClickTab = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.stopPropagation()
     onClick(value)
@@ -43,9 +64,13 @@ function TabCardItem({ value, text, active, onClick }: TabCardItemProps) {
   return (
     <div
       className={cn(
-        'border-b-2 border-b-transparent px-5 pt-3 pb-2 text-base text-gray-900',
-        'hover:cursor-pointer hover:border-b-2 hover:border-b-indigo-300',
-        active ? 'border-b-2 border-b-indigo-700 hover:border-b-indigo-700 font-medium bg-indigo-50' : '',
+        'px-5 pt-3 pb-2 text-sm text-gray-900 hover:cursor-pointer ',
+        vertical
+          ? 'border-l-2 border-l-transparent hover:border-l-2 hover:border-l-indigo-300'
+          : 'border-b-2 border-b-transparent hover:border-b-2 hover:border-b-indigo-300',
+        active ? ' font-medium bg-indigo-50' : '',
+        active && vertical ? 'border-l-2 border-l-indigo-700 hover:border-l-indigo-700' : '',
+        active && !vertical ? 'border-b-2 border-b-indigo-700 hover:border-b-indigo-700' : '',
       )}
       onClick={handleClickTab}
     >
