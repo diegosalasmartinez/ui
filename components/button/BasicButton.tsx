e36c67fb4db2be2a5753e3edf9572d3d7be19bdd
Diego Salas <Diego.Salas@promart.pe>
2023-02-18 20:30:25 -0500

    Add primary, secondary, error and success button style

--- components/button/BasicButton.tsx
+++ components/button/BasicButton.tsx
@@ -2,18 +2,32 @@ import { cn } from '@/utils/style'
 
 interface BasicButtonProps {
   text: string
-  category?: 'primary' | 'secondary'
+  category?: 'primary' | 'secondary' | 'error' | 'success'
 }
 
 export default function BasicButton({ text, category = 'primary' }: BasicButtonProps) {
+  const getColor = () => {
+    switch (category) {
+      case 'primary':
+        return 'bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-300'
+      case 'secondary':
+        return 'bg-gray-200 text-gray-700 hover:bg-gray-300 focus:ring-gray-300'
+      case 'success':
+        return 'bg-green-600 hover:bg-green-700 focus:ring-green-300'
+      case 'error':
+        return 'bg-red-600 hover:bg-red-700 focus:ring-red-300'
+      default:
+        return 'bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-300'
+    }
+  }
+
   return (
     <button
       type='button'
       className={cn(
         'rounded-lg px-4 py-2 text-center text-sm font-medium text-white',
-        category === 'primary' ? 'bg-indigo-600 hover:bg-indigo-700' : 'bg-gray-200 text-gray-700 hover:bg-gray-300',
         'focus:outline-none focus:ring-4',
-        category === 'primary' ? 'focus:ring-indigo-300' : 'focus:ring-gray-300',
+        getColor(),
       )}
     >
       {text}
