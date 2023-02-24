03c509cc4eb3da9d28d908cb73c59a10cca9820d
Jeferson Miranda G <jeferson.miranda@pucp.edu.pe>
2023-02-24 11:39:36 -0500

    Add success, error, info and close icons

---
+++ components/icons/InfoIcon.tsx
@@ -0,0 +1,26 @@
+import { cn } from '@/utils/style'
+
+interface InfoIconProps {
+  fill?: string
+  color?: string
+}
+
+export default function InfoIcon({ fill, color }: InfoIconProps) {
+  return(
+    <svg 
+      xmlns='http://www.w3.org/2000/svg'
+      fill={fill}
+      viewBox='0 0 24 24'
+      stroke-width='1.5'
+      stroke='currentColor'
+      className={cn('h-7 w-7', color)}
+    >
+      <path
+        stroke-linecap='round'
+        stroke-linejoin='round'
+        d='M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z'
+      />
+    </svg>
+
+  )
+}
---
+++ components/icons/SuccessIcon.tsx
@@ -0,0 +1,25 @@
+import { cn } from '@/utils/style'
+
+interface SuccessIconProps {
+  fill?: string
+  color?: string
+}
+
+export default function SuccessIcon({ fill, color }: SuccessIconProps) {
+  return(
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill={fill}
+      viewBox='0 0 24 24'
+      stroke-width='1.5'
+      stroke='currentColor'
+      className={cn('h-7 w-7', color)}
+    >
+      <path
+        stroke-linecap='round'
+        stroke-linejoin='round'
+        d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
+      />
+    </svg>
+  )
+}
