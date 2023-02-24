1b29d8073863b0d67118d7d73a338b06f184f8bd
Diego Salas <Diego.Salas@promart.pe>
2023-02-23 22:56:01 -0500

    Show basic button examples

--- pages/button/index.tsx
+++ pages/button/index.tsx
@@ -1,64 +1,16 @@
-import DefaultLayout from '@/components/layout/DefaultLayout'
+import DefaultLayout from "@/components/layout/DefaultLayout";
+import BasicButtons from "./BasicButtons";
 
 const nav = [
-  { title: 'Basic Buttons', href: 'basic-buttons' },
-  { title: 'Outline Buttons', href: 'outline-buttons' },
-]
+    { title: 'Basic Buttons', href: 'basic-buttons' },
+    { title: 'Outline Buttons', href: 'outline-buttons' },
+  ]
 
 export default function Button() {
   return (
     <DefaultLayout title='UI Design System' nav={nav}>
-      <button
-        type='button'
-        className='mr-2 mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-      >
-        Default
-      </button>
-      <button
-        type='button'
-        className='mr-2 mb-2 rounded-lg border border-gray-200 bg-white py-2.5 px-5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700'
-      >
-        Alternative
-      </button>
-      <button
-        type='button'
-        className='mr-2 mb-2 rounded-lg bg-gray-800 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700'
-      >
-        Dark
-      </button>
-      <button
-        type='button'
-        className='mr-2 mb-2 rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700'
-      >
-        Light
-      </button>
-      <button
-        type='button'
-        className='mr-2 mb-2 rounded-lg bg-green-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'
-      >
-        Green
-      </button>
-      <button
-        type='button'
-        className='mr-2 mb-2 rounded-lg bg-red-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'
-      >
-        Red
-      </button>
-      <button
-        type='button'
-        className='mr-2 mb-2 rounded-lg bg-yellow-400 px-5 py-2.5 text-sm font-medium text-white hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 dark:focus:ring-yellow-900'
-      >
-        Yellow
-      </button>
-      <button
-        type='button'
-        className='mb-2 rounded-lg bg-purple-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900'
-      >
-        Purple
-      </button>
-
-      {/* <BasicButtons />
-      <OutlineButtons />
+      <BasicButtons />
+      {/* <OutlineButtons />
       <TextButtons />
       <IconButtons />
       <FloatingButtons />
