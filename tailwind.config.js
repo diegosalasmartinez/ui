1506bec1fda821fb70e2a8f8075dc720a1d3ba8a
Diego Salas <Diego.Salas@promart.pe>
2023-02-25 00:35:22 -0500

    Set global dark color code

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
   darkMode: 'class',
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './app/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:
+      {
+        'dark': '#000',
+      }
+    },
   },
   plugins: [],
 }
